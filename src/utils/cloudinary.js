96baf1dceb4faa77cd2bd327c786a4291df16412
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-21 15:40:47 +0530

    add a utility function for uploading files on Cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "node:fs"; // for unlinking files from local server on uploaded on cloudinary
+import "dotenv/config";
+import e from "cors";
+
+// configure cloudinary
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const uploadFileOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath){
+            // throw path not found error if path does not exist
+        }
+
+        // upload the file
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto", // identify resource type automatically
+        });
+        console.log("UPLOAD RESPONSE:", response);
+        console.log("Public URL:", response.url);
+        return response;
+    } catch (error) {
+        // If upload fails
+        fs.unlinkSync(localFilePath); // unlink corrupted and maliscious files from local server
+        return null;
+    }
+};
+
+export { uploadFileOnCloudinary };
