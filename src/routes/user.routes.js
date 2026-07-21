3223ea52fb2720dea7bc611dec1438937ad81778
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-21 23:11:48 +0530

    add file upload middleware to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+// add file upload middleware to POST /resgister route for avatar and cover image
+router.post("/register",
+    upload.fields([
+        {
+            name: "avatar", // avatar field ( also provide the same name on frontend )
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
 export default router;
