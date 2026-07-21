9a5457c3c8b6359a96b714645426cc55dac63cc4
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-21 20:32:19 +0530

    Setup multer for file uploads on our local server

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// using multer's disk storage engine to get full control on storing files to disk
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { // file represents the uploaded file
+        cb(null, "/tmp/my-uploads");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now();
+        cb(null, file.fieldname + "-" + uniqueSuffix);
+    },
+});
+
+const upload = multer({ storage });
+
+export { upload };
